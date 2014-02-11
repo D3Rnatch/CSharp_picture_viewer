0d140e19d279ffefb6b7f09914d3f819ac2c28e1
Alexandre Brand <brand@ece.fr>
2014-02-11 12:07:51 +0100

    Model implementation v0

---
+++ pictureViewer_model/pictureViewer_model/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pictureViewer_model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    /*
+     *  Model Interface  
+     * Contains all common functions used and implemented in all Model Classes     * 
+     */
+    private interface Model
+    {
+
+    }
+
+    /*
+     * Picture Class, implements Model
+     * Contains all data referring to any picture stored 
+     * by the application
+     */
+    [Serializable()]
+    private class Picture : Model 
+    {
+
+    }
+
+    /*
+     * Album Class, implements Model
+     * Contains all data referring to an album 
+     */
+    [Serializable()]
+    public class Album : Model 
+    {
+
+    }
+
+    /*
+     * Exception definition of the Model implementation 
+     */
+
+    public class ModelException : Exception
+    {
+
+    }
+}
